58aba4b28b69a6f7cc56097e458ac4b8ff02aa48
Gabriel <gabrielvieira840@gmail.com>
2019-03-22 21:01:06 -0300

    Corrigin 3

--- atvtreinamento3.cpp
+++ atvtreinamento3.cpp
@@ -3,15 +3,28 @@
 
 int main (){
 	int vet[5];
-	int busca;
-	for (int i = 0; i <= 4; i++)
+	int busca = 0;
+	int i = 0;
+	for (i = 0; i <= 4; i++)
 	{
 		printf ("Digite o numero %i do vetor = ",i);
 		scanf ("%i", &vet[i]);
-	}
-	for (int i = 0; i <= 4; i++)
+		if (vet[i] % 2 == 1)
+		{
+			busca++;
+		}
+		
+	} 
+		if (busca>0)
+			printf ("Foram digitados %i numeros impares: ", busca);
+		else 
+			printf ("Nao possui impares\n");
+	for (i = 0; i <= 4; i++)
 	{
 		if (vet[i] % 2 == 1)
-			printf ("%i eh impar\n", vet[i]);	
+		{
+			printf (" %i",vet[i]);
+		}
 	}
 }
+
